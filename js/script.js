738bb7d27727c74ee27fa6f8da5dd7a6c53f0dd3
AdaOkonkwo <ada.okonkwo@kodehauz.com>
2026-01-04 18:48:08 +0100

    minor changes

--- js/script.js
+++ js/script.js
@@ -71,3 +71,10 @@ prvbtn.addEventListener("click", () => {
     previousSlide();
     resetTimer();
 });
+
+
+const formSubmit = document.getElementById('submit');
+
+formSubmit.addEventListener('click', () => {
+    alert("Booking via mail is currently not avaliable. Please book via whatsapp of or via web");
+})
